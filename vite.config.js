a13adee6364cf74e6b97435bc203bc740d079daa
Alberto Arracó <albertoarraco@gmail.com>
2025-12-24 13:10:13 +0100

    Add github action

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Ajusta el base path para GitHub Pages (usuario.github.io/impostor/)
+  base: '/impostor/',
   plugins: [react()],
 });
